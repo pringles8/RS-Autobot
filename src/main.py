dbdfab221d16eeb4e2ae028320dcd208975fb2fd
pringles8 <51466095+pringles8@users.noreply.github.com>
2023-03-17 10:00:25 -0400

    Fidelity Complete & Github ready

--- src/main.py
+++ src/main.py
@@ -1,66 +1,127 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+import undetected_chromedriver as uc
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException
+import os
+from dotenv import load_dotenv
 def fid_buy(stock):
+    '''
+    Fidelity - using selenium to "manually" submit tickets.
+    Process: login -> loop through accounts -> check if they can trade stocks -> open trade ticket
+    and submit -> after looped through accounts and stocks, logout
+
+    TO-DO:
+    - Check if the holding stock already
+    - Currently using last price but could use ask price or alert if large spread/dif between last and
+        ask and give choice
+    '''
+
+    # Exclusions
+    def exclusions():
+        # exclude 401K accounts
+        txt = WebDriverWait(driver, 12).until(EC.element_to_be_clickable((By.XPATH, '//div[@class = "portfolio-card-container__banner"]'))).text
+        if "401K" in txt:
+            return True
+        else:
+            return False
+
+    def fid_buy_modal(stock):
+        # Open Trade Modal
+        try:
+            element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.LINK_TEXT, "Trade")))
+        except StaleElementReferenceException:
+            element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.LINK_TEXT, "Trade")))
+
+        element.click()
+
+        # Loop through stocks
+        for s in stock:
+            # Enter symbol
+            element = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.ID, 'eq-ticket-dest-symbol')))
+            element.send_keys(s[0])
+
+            # Enter number of shares
+            element = WebDriverWait(driver, 12).until(
+                EC.element_to_be_clickable((By.XPATH, '//div[@class = "eqt-quantity__container input-label-in-field"]')))
+            element.send_keys(1)
+
+            # Press buy, shares, day, and limit
+            element = WebDriverWait(driver, 12).until(EC.element_to_be_clickable((By.XPATH, '//div[@class = "pvd3-segment-root pvd-segment--medium"]')))
+            for el in element:
+                if el.text == "Buy" or el.text == "Shares" or el.text == "Limit" or el.text == "Day":
+                    el.click()
+
+            # Input last price
+            last_price = WebDriverWait(driver, 12).until(
+                EC.element_to_be_clickable((By.XPATH, '//div[@class = "last-price"]'))).text
+
+            element = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.ID, 'limit-price')))
+            element.send_keys(last_price)
+
+            # Press buy
+            element = WebDriverWait(driver, 12).until(
+                EC.element_to_be_clickable((By.XPATH, '//div[@class = "eq-ticket__order-entry__actionbtn"]')))
+            element.click()
+
+            element = WebDriverWait(driver, 12).until(
+                EC.element_to_be_clickable((By.XPATH, '//div[@class = "eq-ticket__order-entry__actionbtn"]')))
+            element.click()
+
+        return
+
     driver.get("https://fidelity.com")
-    wait = WebDriverWait(driver, 12)
-    element = driver.find_element(By.CLASS_NAME, "last-child")
+    element = WebDriverWait(driver, 12).until(EC.element_to_be_clickable((By.CLASS_NAME, "last-child")))
     element.click()
-    wait = WebDriverWait(driver, 12)
 
     # Login
-    element = driver.find_element(By.CLASS_NAME, "fs-mask-username")
-    element.send_keys("pringleshelat")
-    wait = WebDriverWait(driver, 2)
-    element = driver.find_element(By.ID, 'password')
-    element.send_keys("Hanuman.1")
-    wait = WebDriverWait(driver, 2)
-    element = driver.find_element(By.ID, 'fs-login-button')
+    element = WebDriverWait(driver, 12).until(EC.element_to_be_clickable((By.CLASS_NAME, "fs-mask-username")))
+    element.send_keys(FIDELITY_USERNAME)
+    element = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.ID, 'password')))
+    element.send_keys(FIDELITY_PASSWORD)
+    element = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.ID, 'fs-login-button')))
     element.click()
-    wait = WebDriverWait(driver, 12)
 
-    # Open Trade Modal
+    # Get all accounts
     try:
-        element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.LINK_TEXT, "Trade")))
+        element = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, '//div[@class = "acct-selector__acct-title"]')))
     except StaleElementReferenceException:
-        element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.LINK_TEXT, "Trade")))
+        element = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, '//div[@class = "acct-selector__acct-title"]')))
 
+    # Loop through accounts
+    for el in element:
+        el.click()
+
+        if not exclusions():
+            fid_buy_modal(stock)
+
+    # Logout
+    element = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, '//div[@class = "pnls last-child"]')))
     element.click()
-    wait = WebDriverWait(driver, 2)
-
-    element = driver.find_element(By.ID, "dest-acct-dropdown")
-    element.click()
-
-    try:
-        element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "ett-acct-sel-list")))
-    except NoSuchElementException:
-        element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "ett-acct-sel-list")))
-
-    #element = driver.find_element(By.CLASS_NAME, "ett-acct-sel-list")
-    #print(element.size)
-    # ['Individual - TOD (Z09446147)', 'ROTH IRA (233798797)', 'Traditional IRA (238697913)', 'Fidelity Bloom ℠ Sp...(*5583)', 'Fidelity Bloom ℠ Save (*1860)']
-    # Class name = ett-acct-sel-list
-    #Accounts = Select(element)
-    #print(Accounts.Options())
-
-    #element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.LINK_TEXT, "Trade")))
-    #element.click()
-    #wait = WebDriverWait(driver, 1)
-    #element = driver.find_element(By.ID, "dest-acct-dropdown")
-    #Accounts = Select(element)
-    #print(Accounts.Options())
-
 
 def robin_buy(stock):
     '''
     Robinhood - using robin_stocks but there is an api.
     Process: login -> get holdings -> buy stock if not already held.
+
+    TO-DO:
+    -
     '''
+
     import robin_stocks.robinhood as r
     #import robin_stocks.robinhood as rs_login
 
-    login = r.authentication.login('pringleshelat@gmail.com','DicksOut4Stonk')
+    login = r.authentication.login(ROBINHOOD_USERNAME, ROBINHOOD_PASSWORD)
 
     holdings = r.build_holdings()
     holdings = holdings.keys()
+
     '''
+    NOTES:
     Returns Dict indexed by ticker.
     {'CTXR': {'price': '1.110000', 'quantity': '11.08893400', 'average_buy_price': '1.2430', 'equity': '12.31', 'percent_change':
     '-10.70', 'equity_change': '-1.474828', 'type': 'stock', 'name': 'Citius Pharmaceuticals', 'id': '1be69e97-a90b-412b-8c49-5d1c
@@ -68,6 +129,7 @@ def robin_buy(stock):
     ': '12.1859', 'equity': '13.37', 'percent_change': '-39.44', 'equity_change': '-8.706551', 'type': 'stock', 'name': 'Palantir
     Technologies', 'id': 'f90de184-4f73-4aad-9a5f-407858013eb1', 'pe_ratio': None, 'percentage': '3.30'},
     '''
+
     cash = r.profiles.load_account_profile()['buying_power']
 
     if stock[0] not in holdings and r.stocks.get_latest_price(stock[0], priceType = 'ask_price') < cash:
@@ -80,25 +142,22 @@ def robin_buy(stock):
 
     r.authentication.logout()
 
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support import expected_conditions as EC
-import undetected_chromedriver as uc
-from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException
-
-# Set up selenium
-#options = webdriver.ChromeOptions()
-#options.add_argument('headless')
-
+# Main
+'''
+TO-DO:
+- Add more brokers
+- Vet by exchange/broker pair
+- Exclusions for brokerage, accounts, etc.
+- Track and sell
+'''
+load_dotenv()
 driver = uc.Chrome()
 
 # Get inputs
 accounts = {}
 RS = set()
+
+# Take inputs
 '''
 print("Enter stock ticker and exchange i.e. 'VTI,Nasdaq'\nTo end input, enter 'done': ")
 val = str(input())
@@ -117,4 +176,4 @@ RS = {('O', 'NYSE')} # For testing purposes
 for stock in RS:
 
     fid_buy(stock)
-    # robin_buy(stock)
+    robin_buy(stock)
