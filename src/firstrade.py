7be17d327fffa083d92ed4c58e846049fdeeb396
pringles8 <51466095+pringles8@users.noreply.github.com>
2023-04-03 13:34:21 -0400

    Element staleness errors fixed for fidelity. Log in and get to positions timing fixed.

--- src/firstrade.py
+++ src/firstrade.py
@@ -7,24 +7,18 @@ from selenium.webdriver.support import expected_conditions as EC
 load_dotenv()
 
 def open_website(driver, wait):
-    driver.get("https://fidelity.com")
-    element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "last-child")))
-    element.click()
+    driver.get("https://www.firstrade.com/content/en-us/welcome")
     return driver, wait
 
 def log_in(wait):
     # Login
-    element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "fs-mask-username")))
-    element.send_keys(os.getenv("FIDELITY_USERNAME"))
+    element = wait.until(EC.element_to_be_clickable((By.ID, "username")))
+    element.send_keys(os.getenv("FIRSTRADE_USERNAME"))
     element = wait.until(EC.element_to_be_clickable((By.ID, 'password')))
-    element.send_keys(os.getenv("FIDELITY_PASSWORD"))
-    element = wait.until(EC.element_to_be_clickable((By.ID, 'fs-login-button')))
+    element.send_keys(os.getenv("FIRSTRADE_PASSWORD"))
+    element = wait.until(EC.element_to_be_clickable((By.ID, 'submit')))
     element.click()
-    time.sleep(3)
-
-    # Click positions new-tab__tab
-    element = wait.until(EC.visibility_of_element_located((By.ID, "portsum-tab-positions")))
-    element.click()
+    time.sleep(1)
 
     return
 
@@ -36,7 +30,7 @@ def log_out(wait):
     return
 
 def get_positions(wait):
-    # Get positions
+    # Get positions posweb-cell-symbol-name pvd-btn btn-anchor
     try:
         element = wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//button[@class = "posweb-cell-symbol-name pvd-btn btn-anchor"]')))
         if isinstance(element, list):
@@ -50,18 +44,17 @@ def get_positions(wait):
 
     return positions
 
-def fid_buy(stocks, stay_open, driver, wait):
+def first_buy(stocks, stay_open, driver, wait):
     '''
-    Fidelity - using selenium to "manually" submit tickets.
+    Firstrade - using selenium to "manually" submit tickets.
     Process: login -> loop through accounts -> check if they can trade stocks -> open trade ticket
     and submit -> after looped through accounts and stocks, logout
 
     TO-DO:
+    - Check if the holding stock already
     - Currently using last price but could use ask price or alert if large spread/dif between last and
         ask and give choice
     '''
-
-    # Exclusions
     def exclusions():
         # exclude 401K accounts
         txt = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@class = "portfolio-card-container__banner"]'))).text
@@ -70,7 +63,7 @@ def fid_buy(stocks, stay_open, driver, wait):
         else:
             return False
 
-    def fid_buy_modal(positions):
+    def first_buy_modal(positions):
         # Open Trade Modal
         element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Trade")))
         element.click()
@@ -113,109 +106,51 @@ def fid_buy(stocks, stay_open, driver, wait):
 
     open_website(driver, wait)
     log_in(wait)
-    time.sleep(5)
-
+    time.sleep(1)
+    ------------------------------------------------------------------------------------------
     # Get all accounts
     accounts = wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//div[@class = "acct-selector__acct-title"]')))
 
     # Loop through accounts
-    for i in range(len(accounts)):
-        account = accounts[i]
+    for account in accounts:
+        account.click()
 
-        if EC.staleness_of(account):
-            accounts = wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//div[@class = "acct-selector__acct-title"]')))
-            account = accounts[i]
+        positions = get_positions(wait)
+        time.sleep(3)
 
         if not exclusions():
-            positions = get_positions(wait)
-            time.sleep(3)
+            first_buy_modal(positions)
 
-            fid_buy_modal(positions)
 
-    print("Bought ", stocks, " in Fidelity")
+print("Bought ", stocks, " in Fidelity")
 
     # Log out
     if not stay_open:
         log_out(wait)
 
-def fid_sell(stocks, stay_open, driver, wait):
+    return
+
+def first_sell(stocks, stay_open, driver, wait):
     '''
-    Fidelity - using selenium to "manually" submit tickets.
+    Firstrade - using selenium to "manually" submit tickets.
     Process: login -> loop through accounts -> check if they can trade stocks -> open trade ticket
     and submit -> after looped through accounts and stocks, logout
 
     TO-DO:
-    -
+    - Check if the holding stock already
+    - Currently using last price but could use ask price or alert if large spread/dif between last and
+        ask and give choice
     '''
 
-    # Exclusions
-    def exclusions():
-        # exclude 401K accounts
-        txt = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@class = "portfolio-card-container__banner"]'))).text
-        if "401K" in txt:
-            return True
-        else:
-            return False
+    driver = uc.Chrome()
+    driver.get("https://www.firstrade.com/content/en-us/welcome")
+    wait = WebDriverWait(driver, 10)
 
-    def fid_sell_modal(positions):
-        # Open Trade Modal
-        element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Trade")))
-        element.click()
-
-        # Loop through stocks
-        for s in stocks:
-            if s in positions:
-                # Enter symbol
-                element = wait.until(EC.element_to_be_clickable((By.ID, 'eq-ticket-dest-symbol')))
-                element.send_keys(s)
-
-                # Enter number of shares
-                element = wait.until(EC.element_to_be_clickable((By.ID, 'eqt-shared-quantity')))
-                element.send_keys(1)
-
-                # Press buy, shares, day, and limit
-                element = wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//div[@class = "pvd3-segment-root pvd-segment--medium"]')))
-                for el in element:
-                    if el.text == "Sell" or el.text == "Shares" or el.text == "Market" or el.text == "Day":
-                        el.click()
-
-                # Press preview and sell
-                element = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@class = "eq-ticket__order-entry__actionbtn"]')))
-                element.click()
-
-                element = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@class = "eq-ticket__order-entry__actionbtn"]')))
-                element.click()
-
-            # Close Modal
-            element = wait.until(EC.element_to_be_clickable((By.XPATH,'//a[@class = "float-trade-container-close dialog-close"]')))
-            element.click()
-
-        return
-
-    if not stay_open:
-        open_website(driver, wait)
-        log_in(wait)
-        time.sleep(5)
-
-    # Get all accounts
-    accounts = wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//div[@class = "acct-selector__acct-title"]')))
-
-    # Loop through accounts
-    for i in range(len(accounts)):
-        account = accounts[i]
-
-        if EC.staleness_of(account):
-            accounts = wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//div[@class = "acct-selector__acct-title"]')))
-            account = accounts[i]
-
-        account.click()
-
-        if not exclusions():
-            positions = get_positions(wait)
-            time.sleep(3)
-
-            fid_sell_modal(positions)
-
-    print("Sold ", stocks, " in Fidelity")
-
-    log_out(wait)
+    # Login
+    element = wait.until(EC.element_to_be_clickable((By.ID, "username")))
+    element.send_keys(os.getenv("FIRSTRADE_USERNAME"))
+    element = wait.until(EC.element_to_be_clickable((By.ID, 'password')))
+    element.send_keys(os.getenv("FIRSTRADE_PASSWORD"))
+    element = wait.until(EC.element_to_be_clickable((By.ID, 'submit')))
+    element.click()
+    return
--- src/main.py
+++ src/main.py
@@ -7,36 +7,12 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from fidelity import fid_buy, fid_sell
 from robhinhood import robin_buy, robin_sell
-
-
-def first_buy(stocks):
-    '''
-    Firstrade - using selenium to "manually" submit tickets.
-    Process: login -> loop through accounts -> check if they can trade stocks -> open trade ticket
-    and submit -> after looped through accounts and stocks, logout
-
-    TO-DO:
-    - Check if the holding stock already
-    - Currently using last price but could use ask price or alert if large spread/dif between last and
-        ask and give choice
-    '''
-
-    driver = uc.Chrome()
-    driver.get("https://www.firstrade.com/content/en-us/welcome")
-    wait = WebDriverWait(driver, 10)
-
-    # Login
-    element = wait.until(EC.element_to_be_clickable((By.ID, "username")))
-    element.send_keys(os.getenv("FIRSTRADE_USERNAME"))
-    element = wait.until(EC.element_to_be_clickable((By.ID, 'password')))
-    element.send_keys(os.getenv("FIRSTRADE_PASSWORD"))
-    element = wait.until(EC.element_to_be_clickable((By.ID, 'submit')))
-    element.click()
-    return
+#from firstrade import first_buy
 
 # Main
 '''
 TO-DO:
+- After-hours capabillity
 - Add quantity and other defaults into env
 - Add more brokers
 - Vet by exchange/broker pair
@@ -91,8 +67,8 @@ if len(buy) > 0 and len(sell) > 0:
 
     fid_buy(buy, stay_open, driver, wait)
     fid_sell(buy, stay_open, driver, wait)
-    #first_buy(buy)
-    #first_sell(buy)
+    #first_buy(buy, stay_open, driver, wait)
+    #first_sell(sell, stay_open, driver, wait)
 
     driver.quit()
 elif len(buy) > 0:
@@ -104,7 +80,7 @@ elif len(buy) > 0:
     wait = WebDriverWait(driver, 12)
 
     fid_buy(buy, stay_open, driver, wait)
-    #first_buy(buy)
+    #first_buy(buy, stay_open, driver, wait)
 
     driver.quit()
 else:
@@ -116,6 +92,6 @@ else:
     wait = WebDriverWait(driver, 10)
 
     fid_sell(sell, stay_open, driver, wait)
-    #first_buy(buy)
+    #first_sell(sell, stay_open, driver, wait)
 
     driver.quit()
